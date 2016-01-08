b59af4d82eb3b0590dc3be02bf4a7238d9416ebb
Diederik Krols <diederik_krols@hotmail.com>
2016-01-08 09:37:49 +0100

    Successful read and display.

---
+++ XamlBrewer.Uwp.ColorSwatchReader/Models/NamedColor.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI;
+
+namespace XamlBrewer.Uwp.ColorSwatchReader.Models
+{
+    public class NamedColor
+    {
+        public NamedColor()
+        {}
+
+        public NamedColor(int red, int green, int blue, string name)
+        {
+            Name = name;
+            Color = Color.FromArgb((byte)255, (byte)red, (byte)green, (byte)blue);
+        }
+
+        public string Name { get; set; }
+
+        public Color Color { get; set; }
+    }
+}
--- XamlBrewer.Uwp.ColorSwatchReader/Views/MainPage.xaml.cs
+++ XamlBrewer.Uwp.ColorSwatchReader/Views/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Mvvm.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -8,6 +9,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage;
 using Windows.Storage.Pickers;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -16,18 +18,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using XamlBrewer.Pcl.ColorSwatchReader;
+using XamlBrewer.Uwp.ColorSwatchReader.Models;
 
 namespace XamlBrewer.Uwp.ColorSwatchReader
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
         }
+
+        public ObservableCollection<NamedColor> Palette { get; set; } = new ObservableCollection<NamedColor>();
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -39,11 +41,17 @@ namespace XamlBrewer.Uwp.ColorSwatchReader
             {
                 try
                 {
-                // User picked a file. 
-                var stream = await file.OpenStreamForReadAsync();
-                var reader = new AcoConverter();
-                var result = reader.ReadPhotoShopSwatchFile(stream);
-                var x = result;
+                    // User picked a file. 
+                    var stream = await file.OpenStreamForReadAsync();
+                    var reader = new AcoConverter();
+                    var swatchColors = reader.ReadPhotoShopSwatchFile(stream);
+                    Palette.Clear();
+                    foreach (var color in swatchColors)
+                    {
+                        var pc = new NamedColor(color.Red, color.Green, color.Blue, color.Name);
+                        Palette.Add(pc);
+                        Log.Error(pc.Color.ToString());
+                    }
                 }
                 catch (Exception ex)
                 {
